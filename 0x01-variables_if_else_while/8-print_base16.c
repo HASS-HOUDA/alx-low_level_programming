b9b196df17dc1718ffad3136a5f1ad404e525fe9
HASS-HOUDA <hassani.houda.93@gmail.com>
2023-06-18 16:31:53 -0700

    c language

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - numbers base sixteen
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+	int i;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+	}
+	for (i = 97 ; i <= 102 ; i++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - combo numbers separated
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+		pucthar(',');
+		pucthar(' ');
+	}
+	pucthar('\n');
+	return (0);
+}
