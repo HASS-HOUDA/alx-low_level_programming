290d9dca7322d1fdaeed8035d0ee44be7ad4ea26
HASS-HOUDA <hassani.houda.93@gmail.com>
2023-06-20 13:32:45 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,12 @@
 /**
  * print_alphabet -  print alphabets in lowercase
  *
- * Return: Always 0 (success)
-*/
+*/ 
 void print_alphabet(void)
 {
 	int n;
 
-	for (n = 97; n  <= 122; n++)
+	for (n = 97; n <= 122; n++)
 	{
 		_putchar(n);
 	}
