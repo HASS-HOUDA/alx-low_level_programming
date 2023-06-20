ef430a819374c4e755fac955429df6b440b056b2
HASS-HOUDA <hassani.houda.93@gmail.com>
2023-06-20 12:07:29 -0700

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
 int main(void)
 {
 
-	putchar(_);
-	putchar(p);
-	pucthar(u);
-	putchar(t);
-	putchar(c);
-	putchar(h);
-	putchar(a);
-	putchar(r);
-	putchar('\n');
+	_putchar(_);
+	_putchar(p);
+	_pucthar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar('\n');
 	return (0);
 }
