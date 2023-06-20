6a1732be00e054d075290aa0f711e3f90d45b07e
HASS-HOUDA <hassani.houda.93@gmail.com>
2023-06-20 11:51:11 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - write _putchar
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char;
+
+	putchar(_);
+	putchar(p);
+	pucthar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	putchar('\n');
+	return (0);
+}
