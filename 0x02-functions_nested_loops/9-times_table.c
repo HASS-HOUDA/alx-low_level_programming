43f10f43912cb73f4574776dfb077302a0a4fc00
HASS-HOUDA <hassani.houda.93@gmail.com>
2023-06-21 16:14:44 -0700

    nested

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,17 +9,18 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar(',');
-		_putchar(' ');
-	}
-	for (mult = 0; mult <= 9; mult++)
-	{
-		prod = num * mult;
+		_putchar(48);
+		for (mult = 0; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = num * mult;
 		if (prod <= 9)
 			_putchar(' ');
 		else
 			_putchar((prod / 10) + 48);
 		_putchar((prod % 10) + 48);
-	}
+		}
 	_putchar((prod % 10) + 48);
+	}
 }
