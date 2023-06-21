0e081a65f3a1c5bf68f3e0233d0f899cd3351052
HASS-HOUDA <hassani.houda.93@gmail.com>
2023-06-21 15:58:02 -0700

    nested

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds to integers
+ *
+ * @a: first input
+ * @b: scond input
+ *
+ * Return: sum of a & b
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
